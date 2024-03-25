a586b5ad1edd0a01dd00461b1c35e280433fe79c
Maximilian Kinney <maxekinney@yahoo.com>
2024-03-25 16:57:41 +0100

    Follow Obj, + example folder+ OutAndTuple

---
+++ Examples/OutAndTuple.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class OutAndTuples : MonoBehaviour
+{
+    void Start()
+    {
+        int i;
+        
+        TestBool(true, out i);
+        Debug.Log(i);
+
+
+
+        Tuple<bool, int> person = Tuple.Create(true, 0);
+
+        TestTuple(person.Item1, person.Item2);
+    }
+
+    public bool TestBool(bool yes, out int i)
+    {
+
+        i = 1;
+        return yes;
+    }
+
+    public (bool yes, int i) TestTuple(bool yes, int i)
+    {
+        yes = false;
+        i = 1;
+        return (yes, i);
+    }
+}
+}
