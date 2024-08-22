22dc10caf4be413fc4dd5dcca822ca991eddc7e5
Max Kinney <maximilian@sunday.gg>
2024-08-22 13:50:41 +0200

    PlaneRaycast update
    override to pass in transform instead of position

--- Helpers/PlaneRaycast.cs
+++ Helpers/PlaneRaycast.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlaneRaycast
 {
-    
+
     public static Vector3 RaycastToPlane(Vector3 planePosition, Vector3 planeNormal)
     {
         // Create a ray from the camera through the mouse position
@@ -25,9 +25,23 @@ public class PlaneRaycast
         return Vector3.zero;
     }
 
+    //Raycast to specific object
+    public static Vector3 RaycastToPlane(Transform plane)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float distance;
+        Plane rayPlane = new Plane(plane.up, plane.position);
+        if (rayPlane.Raycast(ray, out distance))
+        {
+            Vector3 intersectionPoint = ray.GetPoint(distance);
+            return intersectionPoint;
+        }
+        return Vector3.zero;
+
+    }
     public static Vector3 RaycastToPlane(float distanceFromCamera)
     {
-        
+
         // Create a ray from the camera through the mouse position
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         // Calculate the position of the plane based on the distance from the camera along the ray
