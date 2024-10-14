7581684e4eb557308b8082b9ce3eab992de52405
Max Kinney <maximilian@sunday.gg>
2024-10-14 17:15:17 +0200

    Parabola Movement + Notes update + ObjectExtensions

---
+++ Movement/ParabolaMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ParabolaMovement : MonoBehaviour
+{
+    
+    public static async void Movement(Transform obj, Transform target, float arcHeight, float speed, float timeToComplete, UnityEvent FinishEvent = null)
+    {
+
+
+        arcHeight = 1f;
+        Vector3 _startPosition = obj.position;
+
+
+        float distance = Vector3.Distance(_startPosition, target.position);
+
+        float _progress = 0f;
+        // This is one divided by the total flight duration, to help convert it to 0-1 progress.
+        float _stepScale = speed / distance;
+
+        int delayTime = (int)(Time.deltaTime * 1000);
+
+
+
+        //yield return new WaitForSeconds(0.25f);
+        while (_progress < 1.0f)
+        {
+            //.transform.position;
+            // Increment our progress from 0 at the start, to 1 when we arrive.
+            _progress = Mathf.Min(_progress + Time.deltaTime * _stepScale, 1.0f);
+
+            // Turn this 0-1 value into a parabola that goes from 0 to 1, then back to 0.
+            float parabola = 1.0f - 4.0f * (_progress - 0.5f) * (_progress - 0.5f);
+
+            // Travel in a straight line from our start position to the target.        
+            Vector3 nextPos = Vector3.Lerp(_startPosition, target.position, _progress);
+
+            // Then add a vertical arc in excess of this.
+            nextPos.y += parabola * arcHeight;
+
+            // Continue as before.
+            //transform.LookAt(nextPos, transform.forward);
+            obj.position = nextPos;
+
+            Debug.Log("Moving");
+
+            await Task.Delay(delayTime);
+
+        }
+
+
+    }
+}
---
+++ ToLearnFrom/ObjectExtensions.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace BatuDev.Utils.Extensions
+{
+	public static class ObjectExtensions
+	{
+		public static void DestroySelf(this Component comp, bool forceDestroy = false) {
+			if (forceDestroy) {
+				Object.Destroy(comp);
+				return;
+			}
+			
+#if UNITY_EDITOR
+			Object.DestroyImmediate(comp);
+#else
+			Object.Destroy(comp);
+#endif
+		}
+
+		public static void DestroySelf(this GameObject go, bool forceDestroy = false) {
+			if (forceDestroy) {
+				Object.Destroy(go);
+				return;
+			}
+			
+#if UNITY_EDITOR
+			Object.DestroyImmediate(go);
+#else
+			Object.Destroy(go);
+#endif
+		}
+
+		public static void DestroySelf(this Transform tr, bool force = false) {
+#if UNITY_EDITOR
+			if (force) {
+				Object.Destroy(tr.gameObject);
+				return;
+			}
+			Object.DestroyImmediate(tr.gameObject);
+#else
+			Object.Destroy(tr.gameObject);
+#endif
+		}
+
+		public static void DisableSelf(this GameObject go) {
+			go.SetActive(false);
+		}
+
+		public static void DisableSelf(this GameObject go, float disableIn, GameObject coroutineOwner,
+			Action onDisabled = null) {
+			disableIn = Mathf.Clamp(disableIn, 0f, disableIn);
+			if (disableIn == 0f) {
+				go.DisableSelf();
+				return;
+			}
+			
+			IEnumerator DelayedDisable() {
+				yield return new WaitForSeconds(disableIn);
+				
+				go.DisableSelf();
+				onDisabled?.Invoke();
+			}
+
+			coroutineOwner.GetComponent<MonoBehaviour>().StartCoroutine(DelayedDisable());
+		}
+
+		public static void EnableSelf(this GameObject go) {
+			go.SetActive(true);
+		}
+
+		public static void EnableSelf(this GameObject go, float enableIn, GameObject coroutineOwner) {
+			enableIn = Mathf.Clamp(enableIn, 0f, enableIn);
+			
+			IEnumerator DelayedEnable() {
+				yield return new WaitForSeconds(enableIn);
+				
+				go.EnableSelf();
+			}
+
+			coroutineOwner.GetComponent<MonoBehaviour>().StartCoroutine(DelayedEnable());
+		}
+
+		public static Component[] GetAllComponents(this GameObject go) {
+			var toReturn = new List<Component>();
+			toReturn.AddRange(go.GetComponents<Component>().Union(go.transform.GetComponentsInChildren<Component>(true)));
+			return toReturn.ToArray();
+		}
+
+		public static void MoveTowards(this Transform tr, Vector3 dir, float speed) {
+			IEnumerator Move() {
+				while (tr.gameObject.activeInHierarchy) {
+					tr.Translate(dir * (speed * Time.deltaTime), Space.World);
+
+					yield return null;
+				}
+			}
+
+			tr.GetComponent<MonoBehaviour>().StartCoroutine(Move());
+		}
+
+		public static void MoveTowards(this Transform tr, float speed, Vector3 position) {
+			IEnumerator Move() {
+				var dir = (position - tr.position).normalized;
+				while (tr.gameObject.activeInHierarchy) {
+					tr.Translate(dir * (speed * Time.deltaTime), Space.World);
+
+					yield return null;
+				}
+			}
+
+			tr.GetComponent<MonoBehaviour>().StartCoroutine(Move());
+		}
+
+#if UNITY_EDITOR
+		public static void Select(this GameObject obj) {
+			Selection.activeGameObject = obj;
+		}
+
+		public static void Select(this Transform tr) {
+			Selection.activeGameObject = tr.gameObject;
+		}
+#endif
+
+		public static Transform[] Children(this Transform tr) {
+			if (tr.childCount == 0) return null;
+
+			var toReturn = new Transform[tr.childCount];
+			for (var i = 0; i < tr.childCount; i++) {
+				toReturn[i] = tr.GetChild(i);
+			}
+
+			return toReturn;
+		}
+
+		public static Transform[] Parents(this Transform tr) {
+			if (tr.parent == null) return null;
+
+			var ks = 0;
+			var parent = tr.parent;
+			var toReturn = new List<Transform>();
+			while (ks < 1000 && parent != null) {
+				ks++;
+
+				toReturn.Add(parent);
+				
+				parent = parent.parent;
+			}
+
+			return toReturn.ToArray();
+		}
+
+		public static Transform[] Children(this Transform tr, bool includeInactive) {
+			if (tr.childCount == 0) return null;
+
+			var toReturn = tr.GetComponentsInChildren<Transform>(includeInactive);
+
+			return toReturn;
+		}
+
+		public static Transform[] AllChildren(this Transform tr, bool includeInactive) {
+			if (tr.Children() == null) return null;
+
+			return tr.GetComponentsInChildren<Transform>(includeInactive);
+		}
+
+		public static void ClearChildren(this Transform tr) {
+			foreach (var transform in tr.Children()) {
+				transform.DestroySelf();
+			}
+		}
+
+		public static Transform LastChild(this Transform tr) {
+			if (tr.childCount == 0) return null;
+			return tr.GetChild(tr.childCount - 1);
+		}
+
+		public static T GetComponentExtended<T>(this Transform tr) {
+			var p = tr.GetComponent<T>();
+			if (p != null) {
+				return p;
+			}
+
+			var c = tr.GetComponentInChildren<T>();
+			if (c != null) {
+				return c;
+			}
+			
+			return tr.GetComponentInParent<T>();
+		}
+
+		public static T[] GetComponentsExtended<T>(this Transform tr, bool includeInactive = false) {
+			List<T> comps = new List<T>();
+			var p = tr.GetComponentsInParent<T>(includeInactive);
+			if (p != null) {
+				comps.AddRange(p);
+			}
+
+			var c = tr.GetComponentsInChildren<T>(includeInactive);
+			if (c != null) {
+				comps.AddRange(c);
+			}
+
+			if (tr.GetComponents<T>() != null) {
+				comps.AddRange(tr.GetComponents<T>());
+			}
+			
+			return comps.ToArray();
+		}
+
+		public static T GetCopyOf<T>(this Component comp, T other) where T : Component
+		{
+			var type = comp.GetType();
+			if (type != other.GetType()) return null;
+			var flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Default | BindingFlags.DeclaredOnly;
+			var pinfos = type.GetProperties(flags);
+			foreach (var pinfo in pinfos) {
+				if (pinfo.CanWrite) {
+					try {
+						pinfo.SetValue(comp, pinfo.GetValue(other, null), null);
+					}
+					catch { }
+				}
+			}
+			var finfos = type.GetFields(flags);
+			foreach (var finfo in finfos) {
+				finfo.SetValue(comp, finfo.GetValue(other));
+			}
+			return comp as T;
+		}
+	}
+}
