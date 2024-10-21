a5d5199b5d09789f9e9d9d3ba1d0004305d60e2a
Max Kinney <maximilian@sunday.gg>
2024-10-21 10:32:18 +0200

    ParabolicMovement updated + Meshbuilder TLF

--- Movement/ParabolaMovement.cs
+++ Movement/ParabolaMovement.cs
@@ -7,7 +7,7 @@ using UnityEngine.Events;
 public class ParabolaMovement : MonoBehaviour
 {
 
-    public static async void Movement(Transform obj, Transform target, float arcHeight, float speed, UnityEvent FinishEvent = null)
+    public static async void Movement(Transform obj, Transform target, float arcHeight, float speed, float timeToComplete, UnityEvent FinishEvent = null)
     {
 
 
@@ -21,7 +21,7 @@ public class ParabolaMovement : MonoBehaviour
         // This is one divided by the total flight duration, to help convert it to 0-1 progress.
         float _stepScale = speed / distance;
 
-        int delayTime = (int)(Time.deltaTime * 1000);
+        
 
 
 
@@ -47,10 +47,47 @@ public class ParabolaMovement : MonoBehaviour
 
             Debug.Log("Moving");
 
-            await Task.Delay(delayTime);
+            await Task.Yield();
 
         }
 
+        FinishEvent?.Invoke();
+    }
 
+    private IEnumerator ParabolaMove(Transform obj, Transform target, float arcHeight = 1f, float speed = 1f, UnityEvent FinishEvent = null)
+    {
+
+        Vector3 _startPosition = obj.position;
+
+
+        float distance = Vector3.Distance(_startPosition, target.position);
+
+        float _progress = 0f;
+        // This is one divided by the total flight duration, to help convert it to 0-1 progress.
+        float _stepScale = speed / distance;
+
+        while (_progress <= 1.0f)
+        {
+
+            // Increment our progress from 0 at the start, to 1 when we arrive.
+            _progress = Mathf.Min(_progress + Time.deltaTime * _stepScale, 1.0f);
+
+            // Turn this 0-1 value into a parabola that goes from 0 to 1, then back to 0.
+            float parabola = 1.0f - 4.0f * (_progress - 0.5f) * (_progress - 0.5f);
+
+            // Travel in a straight line from our start position to the target.        
+            Vector3 nextPos = Vector3.Lerp(_startPosition, target.position, _progress);
+
+            // Then add a vertical arc in excess of this.
+            nextPos.y += parabola * arcHeight;
+
+            // Continue as before.
+            transform.LookAt(nextPos, transform.forward);
+            transform.position = nextPos;
+
+            yield return null;
+
+        }
+        FinishEvent?.Invoke();
     }
 }
---
+++ ToLearnFrom/MeshBuilder.cs
@@ -0,0 +1,131 @@
+﻿using BatuDev.Utils;
+using UnityEngine;
+
+namespace BatuDev.MeshCreator
+{
+	/// <summary>
+	/// WIP
+	/// </summary>
+	public class MeshBuilder
+	{
+		private float _bezier;
+		
+		public Mesh BuildMesh(Transform tr, Vector3[] positions, Vector3[] controlPoints,
+			float length, int segments = 10) {
+			if (segments < 10)
+				segments = 10;
+
+			var mesh = new Mesh();
+
+			var vertexCount = 2 * (positions.Length + (positions.Length - 1) * (segments - 1));
+			var verts = new Vector3[vertexCount];
+			var tris = new int[(vertexCount - 2) * 3];
+
+			var point = positions[0];
+			var r = (positions[1] - positions[0]).normalized;
+			r =  Vector3.Cross(r, Vector3.down).normalized;
+			for (int i = 0, k = 0, j = 0; i < positions.Length + (positions.Length - 1) * (segments - 1); i++, k += 2) {
+				var right = point + r * length / 2;
+				var left = point - r * length / 2;
+
+				Debug.DrawLine(right, right + Vector3.up * 5f, Color.blue, 5f);
+				Debug.DrawLine(left, left + Vector3.up * 5f, Color.blue, 5f);
+				
+				verts[k] = right;
+				verts[k + 1] = left;
+
+				_bezier += 1f / segments;
+
+				point = MathUtils.QuadraticLerp(positions[j],
+					controlPoints[j],
+					controlPoints[j + 1],
+					positions[j + 1],
+					_bezier);
+
+				if (i != 0 && i % segments == 0) {
+					_bezier = 0f;
+					j++;
+				}
+			}
+			
+			//Set tris
+			for (int k = 0, j = 0; k < tris.Length; k += 6, j += 2) {
+				if (k >= tris.Length) break;
+				
+				tris[k] = j;
+				tris[k + 1] = j + 1;
+				tris[k + 2] = j + 2;
+				tris[k + 3] = j + 1;
+				tris[k + 4] = j + 3;
+				tris[k + 5] = j + 2;
+			}
+
+			// for (var t = 1; t < positions.Length; t++) {
+			// 	_bezier = 0f;
+			// 	
+			// 	var p1 = positions[t - 1];
+			// 	var p2 = positions[t];
+			// 	var c1 = controlPoints[t - 1];
+			// 	var c2 = controlPoints[t];
+			//
+			// 	// Set vertex
+			// 	var point = MathUtils.QuadraticLerp(p1, c1,
+			// 		c2, p2,
+			// 		_bezier);
+			// 	var r = (p2 - p1).normalized;
+			// 	r =  Vector3.Cross(r, Vector3.down).normalized;
+			// 	for (int i = 0, k = (segments * 3 - (segments - 2)) * (t - 1); i < segments; i++, k += 2) {
+			// 		var right = point + r * length / 2;
+			// 		var left = point - r * length / 2;
+			//
+			// 		if (i == segments - 1) {
+			// 			verts[k] = tr.InverseTransformPoint(right);
+			// 			verts[k + 1] = tr.InverseTransformPoint(left);
+			// 		
+			// 			_bezier += 1f / segments;
+			// 			point = MathUtils.QuadraticLerp(p1, c1,
+			// 				c2, p2,
+			// 				_bezier);
+			// 			right = point + r * length / 2;
+			// 			left = point - r * length / 2;
+			// 		
+			// 			verts[k + 2] = tr.InverseTransformPoint(right);
+			// 			verts[k + 3] = tr.InverseTransformPoint(left);
+			// 		} else {
+			// 			verts[k] = tr.InverseTransformPoint(right);
+			// 			verts[k + 1] = tr.InverseTransformPoint(left);
+			// 		}
+			//
+			// 		_bezier += 1f / segments;
+			// 		point = MathUtils.QuadraticLerp(p1, c1,
+			// 			c2, p2,
+			// 			_bezier);
+			// 	}
+			//
+			// 	foreach (var v in verts) {
+			// 		Debug.DrawLine(tr.TransformPoint(v), tr.TransformPoint(v + Vector3.up * 10f), Color.blue, 2f);
+			// 	}
+			//
+			// 	// Set tris
+			// 	for (int i = 0, k = segments * 6 * (t - 1), j = (t - 1) * 2; i < segments; i++, k += 6, j += 2) {
+			// 		tris[k] = j;
+			// 		tris[k + 1] = j + 1;
+			// 		tris[k + 2] = j + 2;
+			// 		tris[k + 3] = j + 1;
+			// 		tris[k + 4] = j + 3;
+			// 		tris[k + 5] = j + 2;
+			// 	}
+			// }
+			//
+			// foreach (var t in tris) {
+			// 	Debug.Log($"{t}");
+			// }
+			
+			// Debug.Log($"{verts.Length}");
+			mesh.vertices = verts;
+			mesh.triangles = tris;
+			
+			return mesh;
+		}
+	}
+}
