1e569dfb0acb70148c5ee54d99e48812fc30a7fe
MaxKinneyCrazyLabs <maxk@crazylabs.com>
2026-02-24 11:13:12 +0100

    Added NeedClick+MouseDown to FollowMouse

--- FollowMouse.cs
+++ FollowMouse.cs
@@ -1,20 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class FollowMouse : MonoBehaviour
 {
+    public bool needClick = false;
     public Transform plane;
+
     private void Update()
     {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(ray.origin, ray.direction * 10f, Color.red, 1f);
+        if (needClick)
+        {
+            return;
+        }
+
         if (plane != null)
         {
             Vector3 manualPos = PlaneRaycast.RaycastToPlane(plane);
             transform.position = manualPos;
             return;
         }
+
+
         Vector3 pos = PlaneRaycast.RaycastToPlane(transform.position, Camera.main.transform.forward);
 
         transform.position = pos;
+    }
+
+
+    public void OnMouseDown()
+    {
+        if (needClick)
+        {
+            needClick = false;
+        }
     }
 }
