ffb58274ff3f4f1fb2f44df63f44abf8bd6368f6
MaxKinneyCrazyLabs <maxk@crazylabs.com>
2026-02-24 11:11:43 +0100

    Follow Mouse giving Manual plane

--- FollowMouse.cs
+++ FollowMouse.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class FollowMouse : MonoBehaviour
 {
+    public Transform plane;
     private void Update()
     {
+        if (plane != null)
+        {
+            Vector3 manualPos = PlaneRaycast.RaycastToPlane(plane);
+            transform.position = manualPos;
+            return;
+        }
         Vector3 pos = PlaneRaycast.RaycastToPlane(transform.position, Camera.main.transform.forward);
 
         transform.position = pos;
